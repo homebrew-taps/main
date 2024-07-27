237750586a7f46a864bf9cacb39f40f753c0cf26
imxieke <oss@live.hk>
2024-07-27 22:34:59 +0800

    update q formula to 0.19.2

--- Formula/q.rb
+++ Formula/q.rb
@@ -1,9 +1,9 @@
 class Q < Formula
   desc "A tiny command line DNS client with support for UDP, TCP, DoT, DoH, DoQ and ODoH."
   homepage "https://github.com/natesales/q"
-  version "0.15.1"
+  version "0.19.2"
   url "https://github.com/natesales/q/releases/download/v#{version}/q_#{version}_darwin_amd64.tar.gz"
-  sha256 "3826f944c073ab68b76fe25687468bbb7604f3e0033362f1deb23b075bdae17b"
+  sha256 "1a3b71d0eb7943f61a983115a79d2b8178ec66d3577430e60662d8e58950762a"
   license "GPL-or-later"
 
   def install
